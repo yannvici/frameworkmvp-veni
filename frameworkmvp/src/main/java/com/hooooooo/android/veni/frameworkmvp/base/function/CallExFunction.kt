0c3741e0ca82a89d84812bb3390a295721247fdd
yann <2472923321@qq.com>
2023-03-24 15:29:44 +0800

    优化代码

--- frameworkmvp/src/main/java/com/hooooooo/android/veni/frameworkmvp/base/function/CallExFunction.kt
+++ frameworkmvp/src/main/java/com/hooooooo/android/veni/frameworkmvp/base/function/CallExFunction.kt
@@ -1,6 +1,8 @@
 package com.hooooooo.android.veni.frameworkmvp.base.function
 
+import com.hooooooo.android.veni.frameworkmvp.Constants
 import com.hooooooo.android.veni.frameworkmvp.exception.HttpStatusCodeException
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
 import retrofit2.Call
 import retrofit2.Callback
@@ -14,19 +16,18 @@ import kotlin.coroutines.resumeWithException
  * Describe:
  */
 
+@OptIn(ExperimentalCoroutinesApi::class)
 suspend fun <T> Call<T>.await(): T = suspendCancellableCoroutine { continuation ->
     continuation.invokeOnCancellation {
         cancel()
     }
     enqueue(object : Callback<T> {
         override fun onResponse(call: Call<T>, response: Response<T>) {
-            try {
-                response.body()?.let {
-                    continuation.resume(it)
+            response.body()?.let {
+                continuation.resume(it) {
+                    continuation.resumeWithException(HttpStatusCodeException(response.raw()))
                 }
-            } catch (t: Throwable) {
-                continuation.resumeWithException(HttpStatusCodeException(response.raw()))
-            }
+            } ?: continuation.resumeWithException(NullPointerException(Constants.UNKNOWN))
         }
 
         override fun onFailure(call: Call<T>, t: Throwable) {
